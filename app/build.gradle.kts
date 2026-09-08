eac7986c5e364556ab541a69023dd72b5e466526
Rajendra Pandey <rajendrapandey199971@gmail.com>
2026-09-08 14:46:08 +0545

    9.13.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,8 +108,8 @@ android {
         applicationId = applicationIdOverride ?: baseApplicationId
         minSdk = 26
         targetSdk = 36
-        versionCode = 155
-        versionName = "9.13.2"
+        versionCode = 156
+        versionName = "9.13.3"
         resValue("string", "app_name", appNameOverride ?: "Blazify")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
