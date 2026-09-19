36e0129deba467a8fa33181a4ea1f99a78afd8c4
Rajendra Pandey <rajendrapandey199971@gmail.com>
2026-09-19 22:09:49 +0545

    Revert "Add a switch for the moving album covers"
    This reverts commit 3fb6700d1787a5b104cc17e39ef7c66ec96f0603.

--- app/src/main/kotlin/com/blazify/music/ui/screens/settings/LookAndFeelScreen.kt
+++ app/src/main/kotlin/com/blazify/music/ui/screens/settings/LookAndFeelScreen.kt
@@ -69,7 +69,6 @@ import coil3.compose.AsyncImage
 import com.blazify.music.LocalPlayerAwareWindowInsets
 import com.blazify.music.LocalPlayerConnection
 import com.blazify.music.R
-import com.blazify.music.constants.AnimatedCoversKey
 import com.blazify.music.constants.AppBarHeight
 import com.blazify.music.constants.DarkModeKey
 import com.blazify.music.constants.DefaultOpenTabKey
@@ -178,7 +177,6 @@ fun LookAndFeelScreen(
         rememberEnumPreference(GridItemsSizeKey, GridItemSize.SMALL)
     var showGridSizeDialog by rememberSaveable { mutableStateOf(false) }
     val (slimNavBar, onSlimNavBarChange) = rememberPreference(SlimNavBarKey, defaultValue = false)
-    val (animatedCovers, onAnimatedCoversChange) = rememberPreference(AnimatedCoversKey, defaultValue = true)
     val (navBarStyle, onNavBarStyleChange) = rememberEnumPreference(NavBarStyleKey, NavBarStyle.PILL)
     var showNavBarStyleDialog by rememberSaveable { mutableStateOf(false) }
     val (showHomeGreeting, onShowHomeGreetingChange) = rememberPreference(ShowHomeGreetingKey, defaultValue = true)
@@ -226,18 +224,6 @@ fun LookAndFeelScreen(
                                         title = { Text(stringResource(R.string.player_slider_style)) },
                                         description = { Text(sliderStyle.label(squigglySlider)) },
                                         onClick = { showSliderStyleDialog = true },
-                                    ),
-                                    Material3SettingsItem(
-                                        icon = painterResource(R.drawable.play),
-                                        title = { Text(stringResource(R.string.animated_covers)) },
-                                        description = { Text(stringResource(R.string.animated_covers_desc)) },
-                                        trailingContent = {
-                                            Switch(
-                                                checked = animatedCovers,
-                                                onCheckedChange = onAnimatedCoversChange,
-                                            )
-                                        },
-                                        onClick = { onAnimatedCoversChange(!animatedCovers) },
                                     ),
                                 ),
                             )
