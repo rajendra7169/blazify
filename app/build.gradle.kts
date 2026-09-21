ba361868794549ce6799b4b7d023a3dd05b44e70
Rajendra Pandey <rajendrapandey199971@gmail.com>
2026-09-21 13:27:03 +0545

    Blazify 9.15.0-beta.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,8 +108,8 @@ android {
         applicationId = applicationIdOverride ?: baseApplicationId
         minSdk = 26
         targetSdk = 36
-        versionCode = 158
-        versionName = "9.15.0-beta.1"
+        versionCode = 159
+        versionName = "9.15.0-beta.2"
         resValue("string", "app_name", appNameOverride ?: "Blazify")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
